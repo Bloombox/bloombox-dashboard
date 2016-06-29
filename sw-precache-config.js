de1b1ff8dde0dd1530b8991cfe4b3398dd4aa86e
Sam Gammon <sam@momentum.io>
2016-06-29 09:42:38 -0700

    Add correct path to precache for webcomponentsjs

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -2,7 +2,7 @@ module.exports = {
   staticFileGlobs: [
     '/index.html',
     '/manifest.json',
-    '/bower_components/webcomponentsjs/webcomponents-lite.min.js'
+    '/assets/ext/components/webcomponentsjs/webcomponents-lite.min.js'
   ],
   navigateFallback: '/index.html'
 };
